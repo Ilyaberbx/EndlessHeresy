6cbb5ba4c4e74598d7c555e41e794de1d9676ea9
DESKTOP-DQ00NHQ\Ilya <ilyaberbx@gmail.com>
2025-06-17 00:00:25 +0300

    Ability Cooldown Tick

--- Assets/Scripts/Runtime/Features/Abilities/AbilitiesCastComponent.cs
+++ Assets/Scripts/Runtime/Features/Abilities/AbilitiesCastComponent.cs
@@ -2,17 +2,30 @@
 using System.Threading;
 using System.Threading.Tasks;
 using EndlessHeresy.Runtime.Data.Identifiers;
+using EndlessHeresy.Runtime.Services.Tick;
 
 namespace EndlessHeresy.Runtime.Abilities
 {
     public sealed class AbilitiesCastComponent : PocoComponent
     {
+        private readonly IGameUpdateService _gameUpdateService;
         private AbilitiesStorageComponent _storage;
+
+        public AbilitiesCastComponent(IGameUpdateService gameUpdateService)
+        {
+            _gameUpdateService = gameUpdateService;
+        }
 
         protected override Task OnPostInitializeAsync(CancellationToken cancellationToken)
         {
             _storage = Owner.GetComponent<AbilitiesStorageComponent>();
+            _gameUpdateService.OnUpdate += OnUpdate;
             return Task.CompletedTask;
+        }
+
+        protected override void OnDispose()
+        {
+            _gameUpdateService.OnUpdate -= OnUpdate;
         }
 
         public async Task<bool> TryCastAsync(AbilityType identifier)
@@ -37,6 +50,17 @@ namespace EndlessHeresy.Runtime.Abilities
             await ability.RootCommand.ExecuteAsync(Owner, DisposalToken);
             ability.SetState(AbilityState.Cooldown);
             return true;
+        }
+
+        private void OnUpdate(float deltaTime)
+        {
+            foreach (var ability in _storage.Abilities)
+            {
+                if (ability.State.Value == AbilityState.Cooldown)
+                {
+                    ability.TickCooldown(deltaTime);
+                }
+            }
         }
 
         private bool HasActiveAbilities()
