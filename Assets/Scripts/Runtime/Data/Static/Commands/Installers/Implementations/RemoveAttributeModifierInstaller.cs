ca3485d6f3b30d4078a35a8aaf914b79e294b505
DESKTOP-DQ00NHQ\Ilya <ilyaberbx@gmail.com>
2025-06-26 15:42:13 +0300

    Attribute Modifier Commands

---
+++ Assets/Scripts/Runtime/Data/Static/Commands/Installers/Implementations/RemoveAttributeModifierInstaller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EndlessHeresy.Runtime.Commands;
+using EndlessHeresy.Runtime.Commands.Attributes;
+using EndlessHeresy.Runtime.Data.Static.Components;
+using UnityEngine;
+using VContainer;
+
+namespace EndlessHeresy.Runtime.Data.Static.Commands.Installers
+{
+    [Serializable]
+    public sealed class RemoveAttributeModifierInstaller : UndoableCommandInstaller
+    {
+        [SerializeField] private AttributeModifierData _data;
+
+        public override IUndoableCommand GetUndoableCommand()
+        {
+            return new RemoveAttributeModifierCommand(_data);
+        }
+
+        public override ICommand GetCommand(IObjectResolver resolver)
+        {
+            return GetUndoableCommand();
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Features/Commands/Implementations/Attributes/RemoveAttributeModifierCommand.cs
@@ -0,0 +1,41 @@
+﻿using EndlessHeresy.Runtime.Attributes;
+using EndlessHeresy.Runtime.Data.Static.Components;
+
+namespace EndlessHeresy.Runtime.Commands.Attributes
+{
+    public sealed class RemoveAttributeModifierCommand : IActorCommand, IUndoableCommand
+    {
+        private readonly AttributeModifierData _data;
+        private IActor _actor;
+
+        public RemoveAttributeModifierCommand(AttributeModifierData data)
+        {
+            _data = data;
+        }
+
+        public void Execute()
+        {
+            if (_actor == null)
+            {
+                return;
+            }
+
+            if (!_actor.TryGetComponent<AttributesComponent>(out var attributes))
+            {
+                return;
+            }
+
+            attributes.Decrease(_data.Identifier, _data.Value);
+        }
+
+        public ICommand GetUndoCommand()
+        {
+            return new AddAttributeModifierCommand(_data);
+        }
+
+        public void Setup(IActor actor)
+        {
+            _actor = actor;
+        }
+    }
+}
